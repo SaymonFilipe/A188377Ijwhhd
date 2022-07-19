b2684f2df489d9858c0c2fa8966bcd938e54fecd
SaymonFilipe <108445021+SaymonFilipe@users.noreply.github.com>
2022-07-19 16:17:57 -0300

    bugs

--- ajhsj12.lua
+++ ajhsj12.lua
@@ -167,423 +167,227 @@ end
 uzu.window.helmet1:setItemId(storage.uzuItems.helmet1)
 
 
-
 uzu.window.armor.onItemChange = function(widget)
-
     storage.uzuItems.armor = widget:getItemId()
-
 end
-
 uzu.window.armor:setItemId(storage.uzuItems.armor)
 
-
-
 uzu.window.armor1.onItemChange = function(widget)
-
     storage.uzuItems.armor1 = widget:getItemId()
-
 end
 
 uzu.window.armor1:setItemId(storage.uzuItems.armor1)
 
-
-
 uzu.window.legs.onItemChange = function(widget)
-
     storage.uzuItems.legs = widget:getItemId()
-
 end
-
 uzu.window.legs:setItemId(storage.uzuItems.legs)
 
-
-
 uzu.window.legs1.onItemChange = function(widget)
-
     storage.uzuItems.legs1 = widget:getItemId()
-
 end
 
 uzu.window.legs1:setItemId(storage.uzuItems.legs1)
 
-
-
 uzu.window.boots.onItemChange = function(widget)
-
     storage.uzuItems.boots = widget:getItemId()
-
 end
-
 uzu.window.boots:setItemId(storage.uzuItems.boots)
 
-
-
 uzu.window.boots1.onItemChange = function(widget)
-
     storage.uzuItems.boots1 = widget:getItemId()
-
 end
-
 uzu.window.boots1:setItemId(storage.uzuItems.boots1)
 
-
-
 uzu.window.shield.onItemChange = function(widget)
-
     storage.uzuItems.shield = widget:getItemId()
-
 end
-
 uzu.window.shield:setItemId(storage.uzuItems.shield)
 
-
-
 uzu.window.shield1.onItemChange = function(widget)
-
     storage.uzuItems.shield1 = widget:getItemId()
-
 end
-
 uzu.window.shield1:setItemId(storage.uzuItems.shield1)
 
-
 uzu.window.shield.onItemChange = function(widget)
-
     storage.uzuItems.shield = widget:getItemId()
-
 end
-
 uzu.window.shield:setItemId(storage.uzuItems.shield)
 
 uzu.window.Ammo.onItemChange = function(widget)
-
     storage.uzuItems.Ammo = widget:getItemId()
-
 end
-
 uzu.window.Ammo:setItemId(storage.uzuItems.Ammo)
 
 uzu.window.Ammo1.onItemChange = function(widget)
-
     storage.uzuItems.Ammo1 = widget:getItemId()
-
 end
-
 uzu.window.Ammo1:setItemId(storage.uzuItems.Ammo1)
 
 uzu.window.ring.onItemChange = function(widget)
-
     storage.uzuItems.ring = widget:getItemId()
-
 end
-
 uzu.window.ring:setItemId(storage.uzuItems.ring)
 
 uzu.window.ring1.onItemChange = function(widget)
-
     storage.uzuItems.ring1 = widget:getItemId()
-
 end
-
 uzu.window.ring1:setItemId(storage.uzuItems.ring1)
 
 uzu.window.weapon.onItemChange = function(widget)
-
     storage.uzuItems.weapon = widget:getItemId()
-
 end
-
 uzu.window.weapon:setItemId(storage.uzuItems.weapon)
 
 uzu.window.weapon1.onItemChange = function(widget)
-
     storage.uzuItems.weapon1 = widget:getItemId()
-
 end
 
 uzu.window.weapon1:setItemId(storage.uzuItems.weapon1)
 
-
 uzu.window.amulet.onItemChange = function(widget)
-
     storage.uzuItems.amulet = widget:getItemId()
-
 end
-
 uzu.window.amulet:setItemId(storage.uzuItems.amulet)
 
-
-
 uzu.window.amulet1.onItemChange = function(widget)
-
     storage.uzuItems.amulet1 = widget:getItemId()
-
 end
-
 uzu.window.amulet1:setItemId(storage.uzuItems.amulet1)
 
-
-
 uzu.window.scroll.onValueChange = function(scroll, value)
-
     storage.uzuItems.scroll = value
-
     uzu.window.scroll:setText(value.. "%")
-
 end
 
 uzu.window.scroll:setValue(storage.uzuItems.scroll or 0)
 
-
-
 setDefaultTab("Tools")
 
 local ui = setupUI([[
-
 Panel
-
   height: 38
 
-
-
   BotSwitch
-
     id: title
-
     anchors.top: parent.top
-
     anchors.left: parent.left
-
     text-align: center
-
     width: 130
-
     !text: tr('Equipper')
 
-
-
   Button
-
     id: settings
-
     anchors.top: prev.top
-
     anchors.left: prev.right
-
     anchors.right: parent.right
-
     margin-left: 3
-
     height: 17
-
     text: Setup
-
 ]])
 
-
-
 uzu.enabled = false
-
 ui.title:setOn(uzu.enabled);
-
 ui.title.onClick = function(widget)
-
     uzu.enabled = not uzu.enabled
-
     widget:setOn(not uzu.enabled);
-
 end
-
 
 ui.title:onClick()
-
 ui.settings.onClick = function(widget)
-
     local u = uzu.window
-
     if u and not u:isVisible() then
-
         u:show()
-
         u:raise()
-
         u:focus()
-
     else
-
         u:hide()
-
     end
-
 end
-
-
 
 macro(100, function()
 
-
-
 if ui.title:isOn() then
 
-
-
 local value = storage.uzuItems.scroll or 0
-
 local set = storage.uzuItems
 
-
-
 local helmet, armor, leg, boots, shield, neck, weapon, ring, Ammo = set.helmet, set.armor, set.legs, set.boots, set.shield, set.amulet, set.weapon, set.ring, set.Ammo
-
-
 local helmet1, armor1, leg1, boots1, shield1, neck1, weapon1, ring1,Ammo1 = set.helmet1, set.armor1, set.legs1, set.boots1, set.shield1, set.amulet1, set.weapon1, set.ring1, set.Ammo1
 
-
-
-
-
     if (hppercent() >= value) then
-
-
-
         if getNeck() == nil or getNeck():getId() ~= neck then
-
             moveToSlot(neck, SlotNeck)
-
         end
-
-
 
         if getHead() == nil or getHead():getId() ~= helmet then
-
             moveToSlot(helmet, SlotHead)
-
         end
-
-
 
         if getBody() == nil or getBody():getId() ~= armor then
-
             moveToSlot(armor, SlotBody)
-
         end
-
-
 
         if getLeg() == nil or getLeg():getId() ~= leg then
-
             moveToSlot(leg, SlotLeg)
-
         end
-
-
 
         if getFeet() == nil or getFeet():getId() ~= boots then
-
             moveToSlot(boots, SlotFeet)
-
         end
 
-
-
         if getRight() == nil or getRight():getId() ~= shield then
-
             moveToSlot(shield, SlotRight)
-
         end
 
         if getLeft() == nil or getLeft():getId() ~= weapon then
-
             moveToSlot(weapon, SlotLeft)
-
         end
 
-
         if getFinger() == nil or getFinger():getId() ~= ring then
-
             moveToSlot(ring, SlotFinger)
-
         end
 
         if getAmmo() == nil or getAmmo():getId() ~= Ammo then
-
             moveToSlot(Ammo, SlotAmmo)
-
         end
-
-
     else
-
-
-
         if getNeck() == nil or getNeck():getId() ~= neck1 then
-
             moveToSlot(neck1, SlotNeck)
-
         end
-
-
 
         if getHead() == nil or getHead():getId() ~= helmet1 then
-
             moveToSlot(helmet1, SlotHead)
-
         end
-
-
 
         if getBody() == nil or getBody():getId() ~= armor1 then
-
             moveToSlot(armor1, SlotBody)
-
         end
-
-
 
         if getLeg() == nil or getLeg():getId() ~= leg1 then
-
             moveToSlot(leg1, SlotLeg)
-
         end
-
-
 
         if getFeet() == nil or getFeet():getId() ~= boots1 then
-
             moveToSlot(boots1, SlotFeet)
-
         end
-
-
 
         if getRight() == nil or getRight():getId() ~= shield1 then
-
             moveToSlot(shield1, SlotRight)
-
         end
-
 
         if getLeft() == nil or getLeft():getId() ~= weapon1 then
-
             moveToSlot(weapon1, SlotLeft)
-
         end
 
-
         if getFinger() == nil or getFinger():getId() ~= ring1 then
-
             moveToSlot(ring1, SlotFinger)
-
         end
 
         if getAmmo() == nil or getAmmo():getId() ~= Ammo1 then
-
             moveToSlot(Ammo1, SlotAmmo)
-
         end
-
     end
-
 end
 
 end)
